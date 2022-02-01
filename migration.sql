7e5ab0fd29ef53013faf56e3344b1933d2b128b6
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2022-02-01 21:02:45 +0400

    hw5

--- migration.sql
+++ migration.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS students CASCADE;
 CREATE TABLE students (id bigserial PRIMARY KEY, name VARCHAR(255), score numeric(6, 0));
-INSERT INTO products (title, price) VALUES
+INSERT INTO students (name, score) VALUES
 ('John Smith', 100),
 ('Jake Gyllenhaal', 80),
 ('Paul Newman', 87),
